ec47285e0e49a1ba062ef29475417f61fe271a21
瘋糖甜薯 <79644863+Shy-fawn@users.noreply.github.com>
2021-05-13 11:02:06 +0800

    123
    11

---
+++ Cyberangel-gh-pages/fable.js
@@ -0,0 +1,41 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+//宣告預設表單內容為空 （你想要的話也可以加東西）
+var initSubject = '',
+  initBody = '';
+
+//按下傳送按鈕後執行
+function submitHandler() {
+  var to = "contact@example.com"; //寫死的傳送對象 就是公司的信箱 不會顯示在網頁上
+  var name = nameText.value; //讀取ID為 nameTextuser 物件中的值
+  var email = emailText.value;
+  var tel = telText.value;
+  var subject = subText.value;
+  //把user填的資料都塞到 mail body 中
+  var body = "" + bodyText.value + '%0A%0A%0A'; //%0A是換行 換了三行
+  body += "From：" + nameText.value + '%0A';
+  body += "Email：" + emailText.value + '%0A';
+  body += "Tel：" + telText.value;
+  //傳送的主要程式碼
+  mailTo.href = "mailto:" + to + "?subject=" + subject + "&body=" + body;
+  mailTo.click();
+}
+//在body onload
+function init() {
+  subText.value = initSubject;
+  toText.value = initTo;
+  bodyText.value = initBody;
+}
